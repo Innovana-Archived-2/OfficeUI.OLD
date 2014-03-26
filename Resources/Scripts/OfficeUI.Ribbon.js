c9ca281b6aafe80721a60c2f3012922470aa1312
Kevin-De-Coninck <kdconinck@hotmail.com>
2014-03-26 21:32:21 +0100

    It's possible to add Dropdown elements right now.

--- Resources/Scripts/OfficeUI.Ribbon.js
+++ Resources/Scripts/OfficeUI.Ribbon.js
@@ -1,6 +1,6 @@
 // Make sure the document is loaded and jQuery is available.
 $(document).ready(function() {
-	// Disable the margin and the padding of the 'UL' class that displays the tabs.
+	// Disable the margin and the padding of the "UL" class that displays the tabs.
 	OfficeUICore.DisableMargin($("#OfficeUI .ribbon .tabs > ul"));
 	OfficeUICore.DisablePadding($("#OfficeUI .ribbon .tabs > ul"));
 
@@ -11,10 +11,10 @@ $(document).ready(function() {
 	// Set the first tab as the active one.
 	OfficeUICore.MakeActive($("#OfficeUI .ribbon .tabs > ul li[role=tab]:not(.application)").first());
 
-	// When clicked on a tab, make sure it's being activated.
+	// When clicked on a tab, make sure it"s being activated.
 	$("#OfficeUI .ribbon .tabs > ul li[role=tab]:not(.application)").click(function() {
 		// Loop over all the tabs and deactivate them.
-		$('#OfficeUI .ribbon .tabs > ul li[role=tab]').each(function(index) {
+		$("#OfficeUI .ribbon .tabs > ul li[role=tab]").each(function(index) {
 			OfficeUICore.MakeInactive(this);
 		});
 
@@ -23,7 +23,7 @@ $(document).ready(function() {
 	});
 
 	// See if an item is marked as a menuitem.
-	// If that's the case, auto add a down arrow to indicate that this is a menuitem.
+	// If that"s the case, auto add a down arrow to indicate that this is a menuitem.
 	$("#OfficeUI .ribbon .tabs > ul li[role=tab] .contents .group .icongroup .icon").children().each(function(index) {
 		if ($(this).hasClass("menu")) {
 
@@ -33,37 +33,31 @@ $(document).ready(function() {
 
 			// Add a click event to the element that contains a menu.
 			$(this).parent().click(function(e) {
-				$(".menu").hide().parent().removeClass("active");
+                e.stopPropagation();
 
-		        // Reset's the state.
-		        $(document).data("state", 0);
-        
-				e.stopPropagation();
-            	
-            	// Check toggle state
-	            if(!$(document).data("state") || $(document).data("state") == 0) {
-	                // If menu is closed, show it
-	                $(element).show().parent().addClass("active");
-
-	                // Update state
-	                $(document).data("state", 1);
-
-	            } else if ($(document).data("state") == 1) {
-	                // If menu is already open, close it
-	                $(element).hide().parent().removeClass('active');
-
-	                // Update state
-	                $(document).data('state', 0);
-	            }
+                // Check toggle state
+                if(!$(this).data("state") || $(this).data("state") == 0) {
+                    // If menu is closed, show it
+                    $(element).show().parent().addClass("active");
+                    
+                    // Update state
+                    $(this).data("state", 1);
+                    
+                } else if ($(this).data("state") == 1) {
+                    // If menu is already open, close it
+                    $(element).hide().parent().removeClass("active");
+                    
+                    // Update state
+                    $(this).data("state", 0);
+                }
+                
 			});
 		}
 	});
-
-	// The following code is executed when a click has been done on the document.
-	$(document).click(function() {
-        $(".menu").hide().parent().removeClass("active");
-
-        // Reset's the state.
-        $(document).data("state", 0);
+    
+    $(document).click(function() {
+        $(".menu").each(function() {
+            $(this).hide().parent().data("state", 0).removeClass("active");
+        });
     });
 });
