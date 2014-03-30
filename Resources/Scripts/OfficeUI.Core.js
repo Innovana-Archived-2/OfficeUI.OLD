88539aa0c946db4079cf02728fbbe68963c3850c
Kevin-De-Coninck <kdconinck@hotmail.com>
2014-03-30 23:24:54 +0200

    Fixed a bug with show/hide menu behind a button.

--- Resources/Scripts/OfficeUI.Core.js
+++ Resources/Scripts/OfficeUI.Core.js
@@ -21,10 +21,23 @@ jQuery.fn.extend({
   // Enable the menu for a given icon.
   //  Parameters:
   //    Timekey: The duration it takes to slide down the element., 
-  EnableMenu: function(time) {
-      $(this).slideDown({
-        duration: time
-      });
+  //    Elementkey: The element on which the "active" class should be set. 
+  EnableMenu: function(time, element) {
+    // Check if the menu is closed. If that's the case, we should show it.
+    if(!$(this).data("state") || $(this).data("state") == 0) { 
+      $(this).show("slide", { direction: "up" }, time);
+      $(element).addClass("active");
+
+      // Update the current state.
+      $(this).data("state", 1);
+    // Menu is visible, so let's close it.
+    } else if ($(this).data("state") == 1) { 
+      $(this).hide();
+      $(element).removeClass("active");
+      
+      // Update the state.
+      $(this).data("state", 0);
+    }
   }
 
 });
@@ -36,10 +49,10 @@ $(document).ready(function() {
 
     // When you click anywhere on the document, execute the following code.
     $(document).click(function() {
-      // Make sure that all dropdowns are closed and that the toggle state is updated.
-        $(".menu").each(function() {
-          $(this).hide();
-        });
+      // Make sure that all dropdowns are closed.
+       $(".menu").each(function() {
+         $(this).hide().parent().removeClass("active");
+       });
     });
 
     // When you click on a tab element (not the first element, since that's the application), make sure the contents are coming available.
@@ -50,11 +63,18 @@ $(document).ready(function() {
     // When you click on an icon execute the code below.
     $(".icon").click(function(e) {
 
-      // When the icon holds a menu, show the mnu.
+      // When the icon holds a menu, show the menu.
       if ($(this).children(".menu").length > 0) {
+
+        $(".menu").each(function() {
+          // Hide the element and update the state.
+          $(this).hide().parent().removeClass("active");
+          $(this).data("state", 0);
+        });
+
         e.stopPropagation();
 
-        $(".menu", this).EnableMenu(75);
+        $(".menu", this).EnableMenu(50, $(this));
       }
     });
 });
