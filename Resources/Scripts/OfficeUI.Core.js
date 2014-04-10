1e242ed02bf9d5b70833dd92a8d09a63be8e83b8
Daniele Di Sarli <danieleds0@gmail.com>
2014-04-10 10:39:52 +0200

    Allow runtime initialization of dynamically added elements

--- Resources/Scripts/OfficeUI.Core.js
+++ Resources/Scripts/OfficeUI.Core.js
@@ -52,62 +52,83 @@ jQuery.fn.extend({
 });
 
 var OfficeUICoreInternal = {
-  AddHandlers: function() {
-  
-    // Events handlers are placed here.
-  
-      // When you click anywhere on the document, execute the following code.
-      $(document).click(function() {
-        // Make sure that all dropdowns are closed.
-         $(".menu").each(function() {
-           $(this).hide().parent().removeClass("active");
-           $(this).data("state", 0);
-         });
-      });
-  
-      // When you click on a tab element (not the first element, since that's the application), make sure the contents are coming available.
-      $("li[role=tab]:not(.application)").click(function() {
-        $(this).EnableTabContents();
-      });
-  
-      // When you click on an icon execute the code below.
-      $(".icon").click(function(e) {
-        // When the icon holds a menu, show the menu.
-        if ($(this).children(".menu").length > 0) {
-  
-          e.stopPropagation();
-  
-          $(".menu", this).EnableMenu(50, $(this));
-  
-        }
-      });
-  }
+	AddGlobalHandlers: function() {
+	
+	  // Events handlers are placed here.
+	
+	    // When you click anywhere on the document, execute the following code.
+	    $(document).click(function() {
+	      // Make sure that all dropdowns are closed.
+	       $(".menu").each(function() {
+	         $(this).hide().parent().removeClass("active");
+	         $(this).data("state", 0);
+	       });
+	    });
+	
+	}
+
 }
 
 var OfficeUICore = {
 
-  // Initialize the OfficeUI controls which are on the page.
-  // The initialization is done by placeing classes automatticly on the necessary elements.
-  // That way the HTML is cleaner.
+	/** Initialize an element and its descendants.
+	 * The initialization is done by placeing classes automatticly on the necessary elements.
+	 * That way the HTML is cleaner.
+	 */
+	InitElement: function(element) {
+		var el = $(element);
+		
+		for (var i = 0; i < 2; i++) {
+			var fun;
+			
+			if(i == 0)
+				fun = function(s) { return el.filter(s); };
+			else
+				fun = function(s) { return el.find(s); };
+			
+			fun(".ribbon, .tabs").addClass("brd_btm_grey");
+			fun(".tabs UL, .menucontents UL").addClass("nopadding nomargin");
+			fun(".menucontents UL LI").addClass("nowrap");
+			fun("li[role='tab']").addClass("inline");
+			
+			// Enable the first tab.
+			fun("li[role='tab']:not(.application)").first().EnableTabContents();
+			
+			fun("li span:first-child").addClass("uppercase");
+			fun("li[role='tab'] .contents").addClass("absolute");
+			fun(".group, .seperator").addClass("relative inline");
+			fun(".icongroup, .smallicon .iconlegend, .imageHolder, .menuItem").addClass("inline");
+			fun(".bigicon").addClass("icon relative inline center");
+			fun(".smallicon").addClass("icon relative");
+			fun(".legend, .menu").addClass("absolute");
+			fun(".arrow").addClass("relative");
+			
+			// Attach handlers
+			
+		    // When you click on a tab element (not the first element, since that's the application), make sure the contents are coming available.
+		    $("li[role=tab]:not(.application)").click(function() {
+		      $(this).EnableTabContents();
+		    });
+		
+		    // When you click on an icon execute the code below.
+		    $(".icon").click(function(e) {
+		      // When the icon holds a menu, show the menu.
+		      if ($(this).children(".menu").length > 0) {
+		
+		        e.stopPropagation();
+		
+		        $(".menu", this).EnableMenu(50, $(this));
+		
+		      }
+		    });
+		}
+	
+	},
+		
+  /** Initialize the OfficeUI controls which are on the page. */
   Init: function() {
-    $(".ribbon, .tabs").addClass("brd_btm_grey");
-    $(".tabs UL, .menucontents UL").addClass("nopadding nomargin");
-    $(".menucontents UL LI").addClass("nowrap");
-    $("li[role='tab']").addClass("inline");
-
-    // Enable the first tab.
-    $("li[role='tab']:not(.application)").first().EnableTabContents();
-
-    $("li span:first-child").addClass("uppercase");
-    $("li[role='tab'] .contents").addClass("absolute");
-    $(".group, .seperator").addClass("relative inline");
-    $(".icongroup, .smallicon .iconlegend, .imageHolder, .menuItem").addClass("inline");
-    $(".bigicon").addClass("icon relative inline center");
-    $(".smallicon").addClass("icon relative");
-    $(".legend, .menu").addClass("absolute");
-    $(".arrow").addClass("relative");
-    
-    OfficeUICoreInternal.AddHandlers();
+	OfficeUICore.InitElement(document);
+    OfficeUICoreInternal.AddGlobalHandlers();
   }
 
 }
