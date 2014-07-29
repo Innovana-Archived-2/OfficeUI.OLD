22f838426de3e3333d7661e29426a3b01fcc4290
Kevin De Coninck <kevindeconinck@outlook.be>
2014-07-29 08:52:30 +0200

    Fully updated to match the current release.

--- Resources/Scripts/Plugins/Modal/OfficeUI.Modal.js
+++ Resources/Scripts/Plugins/Modal/OfficeUI.Modal.js
@@ -1,4 +1,4 @@
-// Makes a grid out of an element.
+// Makes a modal out of an element.
 $.fn.Modal = function (options) {
 	// Specify the default options for the menu plugin.
 	var options = $.extend({});
@@ -20,4 +20,28 @@ $.fn.Modal = function (options) {
     }
 
 	// End of Section: Functions.
+}
+
+// Makes a modal element dissapear.
+$.fn.Unmodal = function (options) {
+    // Specify the default options for the menu plugin.
+    var options = $.extend({});
+
+    // Get the object on which this is called.
+    var object = $(this);
+
+    // Initialize the ribbon.
+    Initialize();
+
+    // Section: Functions.
+        
+        // Initialize the whole grid. This is done by placing classes on the various elements that together form the grid.
+        // This is done to keep the HTML less cluthered.
+    function Initialize() {
+            $(object).removeClass('modalDialog');
+            $(object).css('pointer-events', 'none').css({ opacity: 0 });
+            $(object.hide());
+    }
+
+    // End of Section: Functions.
 }
